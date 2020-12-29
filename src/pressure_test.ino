6ad19fd394243a17fe8b0f8b971e13951422fb97
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-29 14:54:20 +0000

    Add features
    - Start switch
    - Ranged testing
    - LED indication of test result

--- src/pressure_test.ino
+++ src/pressure_test.ino
@@ -13,13 +13,19 @@ counter will start again.
 // Pins
 #define PRESSURE_SENSOR A3
 #define PUMP_CONTROL 1
+#define SWITCH 2
+#define LED 3
 
 // Analog reading for target pressure
 // = 3.7/5.0 * 1024 (10-bit)
 #define TARGET_PRESSURE 758
+#define TARGET_PRESSURE_MIN 500
 
 // Test time for pass result (seconds)
 #define TEST_TIME 20
+
+// Range enable
+#define RANGE_TEST 1
 
 // State variables
 bool at_pressure = false;
@@ -28,10 +34,19 @@ bool result = false;
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("Pressure test waiting for switch input");
+
+  // Wait for switch pin to go low
+  while (digitalRead(SWITCH) == HIGH) {
+    delay(50);
+  }
 
   // Turn on the pump at start-up
   pinMode(PUMP_CONTROL, OUTPUT);
   digitalWrite(PUMP_CONTROL, HIGH);
+
+  pinMode(LED, OUTPUT);
+  pinMode(SWITCH, INPUT);
 }
 
 void loop() {
@@ -40,7 +55,11 @@ void loop() {
   Serial.print("current pressure (voltage): ");
   Serial.println(((float) raw / 1024.0) * 5.0);
 
+#if RANGE_TEST
+  if (raw > TARGET_PRESSURE_MIN && raw < TARGET_PRESSURE) {
+#else
   if (raw < TARGET_PRESSURE) {
+#endif
     // Reset counter once pressure changes from below target to above target
     if (!at_pressure) {
       seconds_at_pressure = 0;
@@ -63,10 +82,14 @@ void loop() {
   Serial.print("result: ");
   if (result) {
     Serial.println("PASSED");
+    digitalWrite(LED, HIGH);
   } else {
     Serial.println("FAILED");
+    digitalWrite(LED, LOW);
   }
 
   // Wait one second
   delay(1000);
 }
+
+// Tone
